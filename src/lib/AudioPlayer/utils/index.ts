0685954f36698f8128ecb966b952b9f8d56622b6
Philip Hetue <philip@furrow.llc>
2022-09-01 10:30:39 -0500

    fix and simplify time display formatting.

--- src/lib/AudioPlayer/utils/index.ts
+++ src/lib/AudioPlayer/utils/index.ts
@@ -1,16 +1,3 @@
 export const displayDuration = (time: number) => {
-    function padZero(v: number) {
-      return v < 10 ? "0" + v : v;
-    }
-    const sec = Math.round(time % 60);
-    const min = Math.round((time / 60) % 60);
-    const hr = Math.round((time / (60 * 60)) % 24);
-    return (
-      padZero(time ? hr : 0) +
-      ":" +
-      padZero(time ? min : 0) +
-      ":" +
-      padZero(time ? sec : 0)
-    );
-  };
-  
+  return new Date(time * 1000).toISOString().substring(11, 19);
+};
